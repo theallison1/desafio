2108699de389c9d4b67cb8afb2ea6368dcca50f8
nicolas espinosa <nico.2011.pr@gmail.com>
2024-03-18 23:11:07 -0300

    add master

---
+++ build/resources/main/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO products(name, price,stock,codecategoria) VALUES('HAMBURGUESA', 138.00,2,'GAS');
+INSERT INTO products(name, price,stock,codecategoria) VALUES('HAMBURGUESA', 138.00,2,'GAS');
+INSERT INTO products(name, price,stock,codecategoria) VALUES('HAMBURGUESA', 138.00,2,'GAS');
+INSERT INTO products(name, price,stock,codecategoria) VALUES('HAMBURGUESA', 138.00,2,'GAS');
+INSERT INTO products(name, price,stock,codecategoria) VALUES('HAMBURGUESA', 138.00,2,'GAS');
+INSERT INTO products(name, price,stock,codecategoria) VALUES('HAMBURGUESA', 138.00,2,'GAS');
+INSERT INTO products(name, price,stock,codecategoria) VALUES('HAMBURGUESA', 138.00,2,'GAS');
